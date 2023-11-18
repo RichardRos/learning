e9b395cf6a9d331ea609eb7dc38bb90319c7eeae
RichardRos <134250244+RichardRos@users.noreply.github.com>
2023-11-18 09:28:26 -0500

    added lesson.py and a .gitignore

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+def addup(number1, number2):
+    result = number1 + number2
+    return result
+
+
+answer = addup(3, 4)
+print(answer)
+
+def multiple(number1, number2):
+    result = number1 * number2
+    return result
+
+answer = multiple(4, 3)
+print(answer)
+
